bde36b62214aa9f8b1be77a0e7877448bf92f608
WAMBUINGUNGA <162411881+WAMBUINGUNGA@users.noreply.github.com>
2024-05-29 14:35:51 +0600

    Update anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+# your code goesclass Anagram:
+    def __init__(self, word):
+        self.word_letters = sorted(x for x in word)
+
+    def match(self, my_list):
+        new_list = []
+        for item in my_list:
+            if self.word_letters == sorted([i for i in item]):
+                new_list.append(item)
+        return new_list here!
